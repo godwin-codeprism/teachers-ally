73995ba0b66ff55f0266e59d8f4387ba51df09a6
Godwin VC <alfa.godwin.omega@gmail.com>
2017-03-13 23:50:32 +0530

    Set up Bootstrap

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,2 @@
-require('../scss/_variables.scss');
-require('../scss/_bootswatch.scss');
-require('../node_modules/jquery/dist/jquery.min.js');
+require('../css/bootstrap.css');
 require('../node_modules/bootstrap/dist/js/bootstrap.min.js');
