2d0187336bfa60d4f978bd0f833303234193c496
godwinvc <godwin@swift.local>
2017-05-01 18:33:07 +0530

    changed Theme

--- endpoints/configure.php
+++ endpoints/configure.php
@@ -14,5 +14,14 @@
         $class = json_decode(file_get_contents($class_file));
         $class[$exam_index] -> settings = $settings;
         file_put_contents($class_file, json_encode($class));
+        return "Columns Updated by ".$username." for class: ".$class_name.", for exam: ".$exam_name;
     }
+
+    function updateSubjects($username, $class_name, $exam_name, $exam_index, $settings){
+        $class_file = "../database/".$username."/".$class_name.".json";
+        $class = json_decode(file_get_contents($class_file));
+        $class[$exam_index] -> settings = $settings;
+        file_put_contents($class_file, json_encode($class));
+        return "Subjects Updated by ".$username." for class: ".$class_name.", for exam: ".$exam_name;
+        }
 ?>
--- scripts/configure.controller.js
+++ scripts/configure.controller.js
@@ -10,29 +10,42 @@ angular.module('teachersAlly')
                     }
                 }
             })
-        $scope.addColumns = function (e) {
+        // angular function to add columns and subjects
+        $scope.addColumnsOrSubjects = function (e) {
             var _this = e.currentTarget,
+                type = _this.dataset.type,
                 cell = new String;
-            $scope.settings.columns.push(cell);
+            $scope.settings[type].push(cell);
             $timeout(function () {
-                $(_this).parent().find('.config-card-list li').eq($scope.settings.columns.length - 1).find('p').focus();
+                $(_this).parent().find('.config-card-list li').eq($scope.settings[type].length - 1).find('p').focus();
             }, 1)
         }
         $scope.postChanges = function (elm) {
-            var text = elm[0].innerText;
-            if (elm[0].dataset.type == "column") {
-                if (text != "") {
-                    $scope.settings.columns[parseInt(elm[0].dataset.index)] = text;
-                } else {
-                    $scope.settings.columns.splice(parseInt(elm[0].dataset.index), 1);
-                }
-                updateColumns($scope.settings);
+            var text = elm[0].innerText,
+                type = elm[0].dataset.type;
+            switch (type) {
+                case "column":
+                    if (text != "") {
+                        $scope.settings.columns[parseInt(elm[0].dataset.index)] = text;
+                    } else {
+                        $scope.settings.columns.splice(parseInt(elm[0].dataset.index), 1);
+                    }
+                    updateSettings($scope.settings, 'updateColumns');
+                    break;
+                case "subject":
+                    if (text != "") {
+                        $scope.settings.subjects[parseInt(elm[0].dataset.index)] = text;
+                    } else {
+                        $scope.settings.subjects.splice(parseInt(elm[0].dataset.index), 1);
+                    }
+                    updateSettings($scope.settings, 'updateSubjects');
+                    break;
             }
         }
 
-        function updateColumns(settings) {
+        function updateSettings(settings, action) {
             $http.post('./endpoints/configure.php', {
-                action: 'updateColumns',
+                action: action,
                 params: [$stateParams.user, $stateParams.class, $stateParams.exam, $scope.exam_index, settings]
             }).then(function (res) {
                 console.log(res.data)
